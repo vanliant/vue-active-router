d3a031e7ccf23931a0698aa6ace61f1b6ad8ae49
taoli <15230817603@163.com>
2020-04-15 20:20:49 +0800

    权限管理页面

--- src/views/page/AdminPage.vue
+++ src/views/page/AdminPage.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        admin
+        非admin用户无法查看该页面，该页面通过动态路由设置。
     </div>
 </template>
 
@@ -15,5 +15,10 @@ export default {
 </script>
 
 <style lang="less" scoped>
-
+div{
+    color: #17233d;
+    font-weight: 500;
+    font-size: 24px;
+    line-height: 32px;
+}
 </style>
